c819368b219665df9f383d83b90f05868276c103
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-08-26 20:23:16 +0200

    yes

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const { PORT, CLIENT_URL } = require('./constants')
+
+
+
+//app start
+const appStart = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`The app is running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+appStart();
