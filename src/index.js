8236903f6fc7183dc8b8d0dc9bac8519a68f2887
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-08-27 19:03:36 +0200

    update 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const app = express()
-const { PORT, CLIENT_URL } = require('./constants')
+const express = require("express");
+const app = express();
+const { PORT, CLIENT_URL } = require("./constants");
+const cookieParser = require("cookie-parser");
 
+//import middleware
+app.use(express.json());
+app.use(cookieParser());
 
+//import routes
+const authRoutes = require("./routes/auth");
+
+//initialize routes
+app.use("/api", authRoutes);
 
 //app start
 const appStart = () => {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,1 +1,13 @@
+const { Router } = require("express");
+const { getUsers, register, login } = require("../controllers/auth");
+const { registerValidation, loginValidation } = require("../validators/auth");
+const {
+  validationMiddleware,
+} = require("../middlewares/validations.midleware");
+const router = Router();
 
+router.get("/get-users", getUsers);
+router.post("/register", registerValidation, validationMiddleware, register);
+router.post("/login", loginValidation, validationMiddleware, login);
+
+module.exports = router;
